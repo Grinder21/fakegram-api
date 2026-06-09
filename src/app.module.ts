a05dc58ee0ec218b7e2f28bdff8bf5a6fcde04b5
Ефремов Михаил <mefremov@bsc.local>
2026-06-09 16:36:49 +0300

    add: add api points

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AlbumsModule } from './albums/albums.module';
+import { PhotosModule } from './photos/photos.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, AlbumsModule, PhotosModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
