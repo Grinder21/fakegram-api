7f13be11f2f4c1539870daf7190b0736abd04daa
Fallden178 <spartawinner280@mail.ru>
2026-06-11 12:01:28 +0300

    add: add description for comments

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Delete } from '@nestjs/common';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  // POST /comments - только с access-token
+  // {body, photoId} -> {comment}
+  // 201 - OK create, 400 - bad request, 401 - unauthorized,
+  // 403 - forbidden, 404 - not found
+  @Post()
+  editComments() {
+    return 'TODO: edit comments';
+  }
+
+  // DELETE /comments/:id - только с access-token
+  // - -> 204
+  // 204 - OK (not content), 400 - bad request, 401 - unauthorized,
+  // 403 - forbidden, 404 - not found
+  @Delete(':id')
+  deleteComment() {
+    return 'TODO: delete comment';
+  }
+}
